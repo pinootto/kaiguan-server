033af623f0f4a8d0d22edec2437248414a8b91b3
giovanni <giovanni@3773f982-d163-4969-ba0b-a5a6dd1c8378>
2014-12-15 07:23:41 +0000

    initial import

---
+++ app/controllers/Application.scala
@@ -0,0 +1,50 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.libs.json.Json
+import play.api.libs.json.Json._
+
+object Application extends Controller {
+  
+  // define the switch status
+  var switchStatus = false
+  
+  // define the POST form
+  val statusForm = Form(
+    single(
+      "status" -> text
+    )
+  )
+
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+  
+  /**
+   * getStatus
+   */
+  def getStatus = Action {
+    
+    Ok(toJson(switchStatus))
+    
+  } // end getStatus
+  
+  
+  /**
+   * setStatus
+   */
+  def setStatus = Action { implicit request =>
+    
+    this.switchStatus = statusForm.bindFromRequest.get.toBoolean
+    
+    Logger debug "switchStatus = " + switchStatus
+    
+    Ok
+    
+  } // end setStatus
+  
+
+} // end Application
